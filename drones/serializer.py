07f44eacbdace0659beb55ec7e879004e889bca4
Emmanuel <jospag4christ@yahoo.com>
2022-11-28 17:05:44 +0100

    Update

--- drones/serializer.py
+++ drones/serializer.py
@@ -38,13 +38,14 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
 
 class DroneSerializer(serializers.HyperlinkedModelSerializer):
     drone_category = serializers.SlugRelatedField(queryset=DroneCategory.objects.all(), slug_field='name')
-
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Drone
         fields = (
             'url',
             'name',
             'drone_category',
+            'owner'
             'manufacturing_date',
             'has_it_competed',
             'inserted_timestamp'
