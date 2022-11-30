db52e959d3a94531f8cbcc78854ade5b6b896615
Emmanuel <jospag4christ@yahoo.com>
2022-11-30 09:36:54 +0100

    Test included

--- drones/tests.py
+++ drones/tests.py
@@ -1,8 +1,23 @@
 from django.test import TestCase
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
+from django.utils.http import urlencode
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from drones.models import DroneCategory
+from drones import views
 
-user = User.objects.get(username="user01")
-token = Token.objects.create(user=user)
-print(token.key)
-# Create your tests here.
+
+class DroneCategoryTests(APITestCase):
+    def post_drone_category(self, name):
+        url = reverse(views.DroneCategoryList.name)
+        data = {'name': name}
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_post_and_get_drone_category(self):
+        new_drone_category_name = 'Hexacopter'
+        response = self.post_drone_category(new_drone_category_name)
+        print("PK {0}".format(DroneCategory.objects.get().pk))
+        assert response.status_code == status.HTTP_201_CREATED
+        assert DroneCategory.objects.count() == 1
+        assert DroneCategory.objects.get().name == new_drone_category_name
